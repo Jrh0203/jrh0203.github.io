1d1f8dc0681ca3eab680c6bdf3a7286f79c6424d
Jrh0203 <herjo345@gmail.com>
2017-09-02 14:05:42 -0500

    node

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
-var express = require('express')
-var app = express()
+// A very basic web server in node.js
+// Stolen from: Node.js for Front-End Developers by Garann Means (p. 9-10)
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var port = 8000;
+var serverUrl = "127.0.0.1";
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+var http = require("http");
+var path = require("path");
+var fs = require("fs");
+var checkMimeType = true;
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+console.log("Starting web server at " + serverUrl + ":" + port);
+
+http.createServer( function(req, res) {
+
+	var now = new Date();
+
+	var filename = req.url || "index.html";
+	var ext = path.extname(filename);
+	var localPath = __dirname;
+	var validExtensions = {
+		".html" : "text/html",
+		".js": "application/javascript",
+		".css": "text/css",
+		".txt": "text/plain",
+		".jpg": "image/jpeg",
+		".gif": "image/gif",
+		".png": "image/png",
+		".woff": "application/font-woff",
+		".woff2": "application/font-woff2"
+	};
+
+	var validMimeType = true;
+	var mimeType = validExtensions[ext];
+	if (checkMimeType) {
+		validMimeType = validExtensions[ext] != undefined;
+	}
+
+	if (validMimeType) {
+		localPath += filename;
+		fs.exists(localPath, function(exists) {
+			if(exists) {
+				console.log("Serving file: " + localPath);
+				getFile(localPath, res, mimeType);
+			} else {
+				console.log("File not found: " + localPath);
+				res.writeHead(404);
+				res.end();
+			}
+		});
+
+	} else {
+		console.log("Invalid file extension detected: " + ext + " (" + filename + ")")
+	}
+
+}).listen(port, serverUrl);
+
+function getFile(localPath, res, mimeType) {
+	fs.readFile(localPath, function(err, contents) {
+		if(!err) {
+			res.setHeader("Content-Length", contents.length);
+			if (mimeType != undefined) {
+				res.setHeader("Content-Type", mimeType);
+			}
+			res.statusCode = 200;
+			res.end(contents);
+		} else {
+			res.writeHead(500);
+			res.end();
+		}
+	});
+}
